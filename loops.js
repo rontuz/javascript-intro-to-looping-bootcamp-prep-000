bd4d4a5581c48c4139c44f6d20838648a21d5556
Ronald Tuzo <rontuz@icloud.com>
2018-06-14 17:09:19 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+
+function forLoop(array){
+  for (let i=0; i<25; i++){
+      if (i==1){ 
+        array.push("I am 1 strange loop");
+      }else{
+        array.push("I am ${i} strange loops"); }}
+    return array;
+}
+
+function whileLoop(n){
+  let i=n;
+  while (i > 0){
+    console.log(--i);
+  } return "done";  
+  } 
+function doWhileLoop(array){
+   function maybeTrue(){
+     array.length-1 > 0 && maybeTrue; 
+    array.pop();
+}return array;
+}
